72bb5a9c5fb78f17c5ab0e458b66d2bd3518992c
Toufiqul Islam Imran <imran@erainfotechbd.com>
2025-07-08 15:18:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// App.jsx
+import SideNavBar from './component/SideNavBar';
+
+function App() {
+  return <SideNavBar />;
+}
+
+export default App;
---
+++ src/pages/Inbox.jsx
@@ -0,0 +1,4 @@
+
+export default function Inbox() {
+  return <h2>📥 Inbox Page</h2>;
+}
